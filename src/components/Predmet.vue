fcceabcd79a07e162e9c1fcf2ef24c0cdc64fcb0
Ivan <fazlic.ivan00@gmail.com>
2023-12-10 23:01:51 +0100

    Prikaz predmeta gotov

--- src/components/Predmet.vue
+++ src/components/Predmet.vue
@@ -1,11 +1,16 @@
 <script setup>
-
+const props = defineProps(["data"])
 </script>
 
 <template>
-    
+    <tr>
+        <td>{{ props.data.idPredmeta }}</td>
+        <td>{{ props.data.idProfesora }}</td>
+        <td>{{ props.data.naziv }}</td>
+        <td>{{ props.data.espb }}</td>
+        <td>{{ props.data.status }}</td>
+    </tr>
 </template>
 
 <style scoped>
-
 </style>
--- src/components/Predmeti.vue
+++ src/components/Predmeti.vue
@@ -1,31 +1,50 @@
 <script setup>
-import {reactive, ref, watch} from 'vue'
+import { reactive, ref, watch } from 'vue'
 import axios from 'axios';
+import Predmet from './Predmet.vue'
 const props = defineProps(["data"])
 let sviPredmeti = ref([])
 let predmetiStudenta = ref([])
+let predmeti = ref([])
 
-const dohvatiPredmeteStudenta =async ()=>{
+const dohvatiPredmeteStudenta = async () => {
     const filtriraniStudenti = await axios.get(`http://pabp.viser.edu.rs:8000/api/StudentPredmets`)
-    predmetiStudenta.value = await filtriraniStudenti.data.filter(predmet=>predmet.idStudenta == props.data.values.idStudenta)
+    predmetiStudenta.value = await filtriraniStudenti.data.filter(predmet => predmet.idStudenta == props.data.values.idStudenta)
     const sviPredmeti = await axios.get(`http://pabp.viser.edu.rs:8000/api/Predmets`)
     sviPredmeti.value = await sviPredmeti.data
-    console.log(sviPredmeti.value);
-    console.log(predmetiStudenta.value);
+
+    predmeti.value = sviPredmeti.value.filter(objekat2 =>
+        predmetiStudenta.value.some(objekat1 => objekat1.idPredmeta === objekat2.idPredmeta)
+    );
 }
-
-
-watch(props,()=>{
+watch(props, () => {
     dohvatiPredmeteStudenta()
 })
 </script>
 
 <template>
-    <div>
+    <div v-if="predmetiStudenta.length">
         <h2>Predmeti</h2>
+        <table>
+        <thead>
+            <th>ID predmeta</th>
+            <th>ID profesora</th>
+            <th>Naziv predmeta</th>
+            <th>Broj ESPB</th>
+            <th>Status predmeta</th>
+        </thead>
+        <tbody>
+            <Predmet v-for="p in predmeti" :data="p"></Predmet>
+        </tbody>
+    </table>
     </div>
 </template>
 
 <style scoped>
-
+table{
+    text-align: center;
+}
+th{
+    padding: 0 5px;
+}
 </style>
